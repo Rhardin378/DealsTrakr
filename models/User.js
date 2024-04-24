007bcf6ffcf7ad9c0ad2e186c214d6faea67a534
Scott <spalocsik@gmail.com>
2024-04-23 20:34:12 -0400

    updated User and Deal schemas

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  username: { type: String }
+  username: { type: String, required: true },
+  deals: []
 });
 
 const User = mongoose.model("User", UserSchema);
