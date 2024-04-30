8d73c7012ab11ebae43a0b84de194bef66d7cec0
Ryan Hardin <rhardin378@gmail.com>
2024-04-30 12:23:02 -0500

    added styiling to companies page and changes to styling for the tabs panes

--- dealtrakr/app/components/NavBar.js
+++ dealtrakr/app/components/NavBar.js
@@ -4,10 +4,9 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="d-flex justify-content-between container-fluid ">
-        <span className="navbar-brand" href="#">
-          DealsTrakr
-        </span>
-
+        <Link href="/dashboard" className="navbar-brand">
+          <span className="">DealsTrakr</span>
+        </Link>
         <div>
           <Link className="navbar-nav nav-item" href="/">
             <span>Sign In</span>
--- dealtrakr/app/dashboard/page.js
+++ dealtrakr/app/dashboard/page.js
@@ -13,26 +13,14 @@ export default function Dashboard() {
 
   return (
     <main>
-      {/* links components */}
-      <div>
-        {/* <Link href="/companies">Companies</Link>
-        <Link href="/deals">Deals</Link>
-        <Link href="/dashboard">Dashboard</Link> */}
-        {/* <Link href="/addcompany">Add New Company</Link> */}
-      </div>
-
-      <Tabs
-        defaultActiveKey="companies"
-        id="uncontrolled-tab-example"
-        className="mb-3"
-      >
-        <Tab eventKey="companies" title="Companies">
-          <CompaniesListView />{" "}
+      <Tabs defaultActiveKey="companies" id="tab-example" className=" ms-3">
+        <Tab eventKey="companies" title="Companies" className="tabsClass">
+          <CompaniesListView />
         </Tab>
-        <Tab eventKey="deals" title="Deals">
+        <Tab eventKey="deals" title="Deals" className="tabsClass">
           Tab content for Deals
         </Tab>
-        <Tab eventKey="dashboard" title="Dashboard">
+        <Tab eventKey="dashboard" title="Dashboard" className="tabsClass">
           Tab content for Dashboard
         </Tab>
       </Tabs>
