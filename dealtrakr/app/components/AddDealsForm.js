156461a7dd03948fd695f6f7a954676e33e548bd
Scott <spalocsik@gmail.com>
2024-05-08 16:20:24 -0400

    added an option to choose which company the deal is associated with

--- dealtrakr/app/components/AddDealsForm.js
+++ dealtrakr/app/components/AddDealsForm.js
@@ -1,20 +1,26 @@
-"use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { addDeal } from "../store/slices/addDealSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchDeals } from "../store/slices/deals";
+import { fetchCompanies } from "../store/slices/companies"; // Import the fetchCompanies action creator
 
 const AddDealsForm = () => {
   const [show, setShow] = useState(false);
-  const [id, setId] = useState("");
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
   const [dateClosed, setdateClosed] = useState("");
   const [dateInitiated, setDateInitiated] = useState("");
   const [stage, setStage] = useState("");
+  const [selectedCompany, setSelectedCompany] = useState("");
   const dispatch = useDispatch();
+  const companies = useSelector((state) => state.companies.companiesToShow); // Corrected selector for companies
+
+  useEffect(() => {
+    // Fetch companies when component mounts
+    dispatch(fetchCompanies());
+  }, [dispatch]); // Dependency array to ensure useEffect runs only once
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -28,12 +34,12 @@ const AddDealsForm = () => {
     e.preventDefault();
     dispatch(
       addDeal({
-        id,
         name,
         amount,
         dateClosed,
         dateInitiated,
         stage,
+        company: selectedCompany
       })
     );
     handleClose();
@@ -58,49 +64,56 @@ const AddDealsForm = () => {
           <Modal.Title>Add Deal</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <div className="add-deal-form-container">
-          <form>
-            <label className="form-label-mb2">
-              Name:
+          <div className="add-deal-form-container">
+            <form>
+              <label className="form-label-mb2">
+                Name:
+                <input
+                  className="form-control"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </label>
+              <label className="form-label mb-2">Deal Amount:</label>
               <input
                 className="form-control"
                 type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
               />
-            </label>
-            <label className="form-label mb-2">Deal Amount:</label>
-            <input
-              className="form-control"
-              type="text"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-            />
-            <label className="form-label mb-2">Date Initiated:</label>
-            <input
-              type="date"
-              value={dateInitiated}
-              onChange={(e) => setDateInitiated(e.target.value)}
-            />
-            <label>Date Closed:</label>
-            <input
-              className="form-control w-100"
-              type="date"
-              value={dateClosed}
-              onChange={(e) => setdateClosed(e.target.value)}
-            />
-            <label className="form-label mb-2">
-              Stage:
-              <select value={stage} onChange={handleStage}>
-                <option value="initiated">Initiated</option>
-                <option value="qualified">Qualified</option>
-                <option value="contract sent">Contract Sent</option>
-                <option value="closed won">Closed Won</option>
-                <option value="closed lost">Closed Lost</option>
+              <label className="form-label mb-2">Date Initiated:</label>
+              <input
+                type="date"
+                value={dateInitiated}
+                onChange={(e) => setDateInitiated(e.target.value)}
+              />
+              <label>Date Closed:</label>
+              <input
+                className="form-control w-100"
+                type="date"
+                value={dateClosed}
+                onChange={(e) => setdateClosed(e.target.value)}
+              />
+              <label className="form-label mb-2">
+                Stage:
+                <select value={stage} onChange={handleStage}>
+                  <option value="initiated">Initiated</option>
+                  <option value="qualified">Qualified</option>
+                  <option value="contract sent">Contract Sent</option>
+                  <option value="closed won">Closed Won</option>
+                  <option value="closed lost">Closed Lost</option>
+                </select>
+              </label>
+              <label>Select Company:</label>
+              <select value={selectedCompany} onChange={(e) => setSelectedCompany(e.target.value)}>
+                <option value="">Select a company</option>
+                {companies.map((company) => (
+                  <option key={company._id} value={company._id}>{company.name}</option>
+                ))}
               </select>
-            </label>
-          </form>
-        </div>
+            </form>
+          </div>
         </Modal.Body>
         <Modal.Footer className="modal-footer">
           <Button variant="secondary" onClick={handleClose}>
